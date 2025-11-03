88d0760b3aec5b8216a374c437eeccf9c955aadf
swiipw <sandra.conde.cuevas@gmail.com>
2025-11-02 19:05:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import LoginScreen from './screens/LoginScreen'; // Importamos la pantalla de Login
+import LoginScreen from 'screens/LoginScreen'; // Importamos la pantalla de Login
 import MainAppScreen from './MainAppScreen'; // Importamos la aplicación principal
 
 // --- Componente Principal: App ---
@@ -7,9 +7,9 @@ export default function App() {
   const [user, setUser] = useState(null);
 
   if (user) {
-    return <MainAppScreen user={user} onLogout={() => setUser(null)} />; [cite: 129, 130]
+    return <MainAppScreen user={user} onLogout={() => setUser(null)} />;
   } else {
-    return <LoginScreen onLogin={(userData) => setUser(userData)} />; [cite: 130, 131]
+    return <LoginScreen onLogin={(userData) => setUser(userData)} />;
   }
 }
 
@@ -18,11 +18,11 @@ window.tailwind.config = {
   theme: {
     extend: {
       colors: {
-        'workly-primary': '#17202A',   
-        'workly-secondary': '#1ABC9C', 
-        'workly-accent': '#F39C12',    
-        'workly-light-bg': '#85C1E9',  
-        'workly-orange': '#E67E22'     
+        'workly-primary': '#17202A',
+        'workly-secondary': '#1ABC9C',
+        'workly-accent': '#F39C12',
+        'workly-light-bg': '#85C1E9',
+        'workly-orange': '#E67E22'
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
@@ -30,16 +30,16 @@ window.tailwind.config = {
       keyframes: {
         'fade-in-up': {
           '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' }, [cite: 132]
+          '100%': { opacity: '1', transform: 'translateY(0)' },
         },
         'bounce': {
-            '0%, 100%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' }, [cite: 132]
-            '50%': { transform: 'none', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)' }, [cite: 132]
+          '0%, 100%': { transform: 'translateY(-25%)', animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)' },
+          '50%': { transform: 'none', animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)' },
         }
       },
       animation: {
         'fade-in-up': 'fade-in-up 0.3s ease-out forwards',
-        'bounce': 'bounce 1s infinite', [cite: 133]
+        'bounce': 'bounce 1s infinite',
       },
     },
   },
