c64302c9aa6881ce276eba73fd8793625c817ea2
swiipw <sandra.conde.cuevas@gmail.com>
2025-11-03 22:19:46 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Importa el componente principal Workly
+import App from './App'; // Importa el componente principal
+// Si tienes un archivo CSS/Tailwind, importalo aquí. Ejemplo:
+// import './index.css'; 
 
-// Configura React para renderizar tu componente principal (<App />) dentro del div con id="root"
+// Configura React para renderizar tu componente principal (<App />)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
